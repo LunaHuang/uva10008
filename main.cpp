ccce7b6dbee95127db39c1aafdaa1242651d8d51
Luna Huang <lunahuang@weintek.com>
2022-03-16 17:46:45 +0800

    First time commit

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <regex>
+#include <cctype>
+#include <boost/range/algorithm/remove_if.hpp>
+//#include <boost/lexical_cast.hpp>
+
+#if !defined(WITH_GTEST)
+#   include <gtest/gtest.h>
+#endif
+
+struct Node
+{
+   char letter;
+   int count;
+   struct Node *next;
+};
+
+/* insert new node at the end of the linked list */
+void append(struct Node** head, char letter, int count)
+{
+	struct Node* newNode = new Node;
+	struct Node *last = *head;
+ 
+	newNode->letter = letter;
+	newNode->count = count;
+	newNode->next = nullptr;
+ 
+	if (*head == nullptr)
+	{
+		*head = newNode;
+		return;
+	}
+
+	while (last->next != nullptr){
+			last = last->next;
+	}
+
+	last->next = newNode;
+	return;
+}
+
+void displayList(struct Node *node)
+{
+	while (node != nullptr)
+	{
+		std::cout<< node->letter << " "<<node->count<<std::endl;
+		node = node->next;
+	}
+ 
+	if(node== nullptr)
+		std::cout<<"null"; 
+}
+
+void order_serial(const std::string &input)
+{
+	int n = input.length();
+    char char_array[n + 1];
+ 
+    strcpy(char_array, input.c_str());
+	struct Node* head = NULL;
+    int count = 1;
+    for (int i = 1; i < n; i++){
+		if(char_array[i] == char_array[i-1]){
+			count++;
+			if( i == (n-1))
+				append(&head, char_array[i-1], count);
+		}else{
+			append(&head, char_array[i-1], count);
+			count = 1;
+			if( i == (n-1))
+				append(&head, char_array[i], count);
+		}
+    //    cout << char_array[i];
+	}
+	displayList(head);
+}
+
+std::string crypt_analysis(const std::string &original_input)
+{
+	std::regex rx("\[^[:alpha:]]+");
+	std::string input = original_input;
+	std::string result;
+	std::regex_replace(std::back_inserter(result),input.begin(),input.end(),rx,"");
+    std::transform(result.begin(), result.end(), result.begin(),toupper);
+	return result;
+}
+
+void solve_uva_problem(std::istream &is)
+{
+    std::string input;
+	std::getline(is, input);	
+    if (input == "0")
+		return;
+
+    std::string analysis_string;
+//	std::cout<<std::endl;
+	int row = std::stoi(input);
+	while(row != 0){
+		input.clear();
+		std::getline(is, input);	
+		//std::cin.getline(input_string, sizeof(input_string));
+        //input.assign(input_string, sizeof(input_string));
+		analysis_string += crypt_analysis(input);
+		row--;
+	}
+	
+	std::sort(analysis_string.begin(), analysis_string.end());
+	std::cout << "match :" << analysis_string << std::endl;
+	order_serial(analysis_string);
+}
+
+int main(int argc, char ** argv)
+{
+#if defined(WITH_GTEST)
+    solve_uva_problem(std::cin);
+#else
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+#endif
+}
---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <string>
+//include "main.cpp"
+/*
+TEST(testCase, StrLessSort)
+{
+    std::string number = std::to_string(23145);
+    EXPECT_STREQ("12345", StrLessSort(number).c_str());
+}
+
+TEST(testCase, StrReverse)
+{
+    std::string number = std::to_string(23145);
+    EXPECT_STREQ("54132", StrReverse(number).c_str());
+}
+*/
